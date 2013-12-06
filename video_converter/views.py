e6ff59251a9a4efcba3785f5a4698cbf870ffd96
alexm92 <the_snyper06@yahoo.com>
2013-12-06 12:39:37 +0000

    first view

--- video_converter/views.py
+++ video_converter/views.py
@@ -1,1 +1,8 @@
-from django.http import
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def home(request):
+    # View code here...
+    t = loader.get_template('templates/base.html')
+    c = RequestContext(request, {'foo': 'bar'})
+    return HttpResponse(t.render(c))

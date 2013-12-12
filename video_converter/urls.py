aa0a10c277062b673e41659f0ffa9cfb49682ba2
alexm92 <the_snyper06@yahoo.com>
2013-12-12 20:56:39 +0000

    Upload files to s3

--- video_converter/urls.py
+++ video_converter/urls.py
@@ -10,5 +10,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'video_converter.views.home'),
+
+    url(r'^upload/$', 'video_converter.views.upload'),
+    url(r'^s3direct/', include('s3direct.urls')),
 )
 
